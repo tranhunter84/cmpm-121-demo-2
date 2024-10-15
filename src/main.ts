8960abe96995f2a5a330e636d69279ba894f774b
tranhunter84 <tranhunter84@gmail.com>
2024-10-15 14:48:33 -0700

    Step 1 completed

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,12 @@ const APP_NAME = "HUNTER'S DEMO #2";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const title = document.createElement("h1");
+title.textContent = APP_NAME;
+app.appendChild(title);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.appendChild(canvas);
